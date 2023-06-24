0e212e6388670d246f325769b289ec30938d8e93
PabloBautistaGoyoneche <135397394+PabloBautistaGoyoneche@users.noreply.github.com>
2023-06-24 18:07:41 -0500

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,20 @@
 #ifndef main_h
 #define main_h
 /**
- * main - check the code*
+ * main - check the code
+ *
  * Return: 0 (Success)
  */
 int _islower(int c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _putchar(char c);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+int add(int, int);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
 int _isalpha(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _putchar(char c);
 #endif
