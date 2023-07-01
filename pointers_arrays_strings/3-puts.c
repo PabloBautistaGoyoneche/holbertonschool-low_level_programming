057db80f5111ef02fb931d7fcaf68cd5e2a5029e
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-06-30 20:01:26 -0700

    Function that prints a string, followed by a newline, to stdout

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Entry point of program
+ * @str: parameter of function
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
